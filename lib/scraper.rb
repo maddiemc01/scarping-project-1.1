85d151c249a56e96867ca6aa78ab6b9f5c0d0eba
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-20 12:34:34 -0400

    scrape category instance

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,23 +12,41 @@ class Scraper
     end
   end
     # scrapes all the articles under category instance
-  # def scrape_category(category)
-  #   page = Nokogiri::HTML(HTTParty.get(category.url).body)
-  #   category.articles = all_articles(page)
-  #     binding.pry
-  # end
+  def scrape_category(categ)
+    page = Nokogiri::HTML(HTTParty.get(categ.url).body)
+    categ.featart = featurearts(page)
+    categ.otherart = otherarts(page)
+  end
 
-  # def all_articles(page)
-  #   page.css(".feed-card").each do |artinfo|
-  #     title = artinfo.css(" .feed-card--title a").text
-  #     author = artinfo.css(".contributor-byline--name").text
-  #     link = artinfo.css(" .feed-card--title a").attr("href").text
-  #     Article.new(title, author, link)
-  #   end
-  # end
+  end
+
+  def featurearts(page)
+    page.css(".four-story--item").each do |info|
+      title = info.css(".four-story--title a").text
+      link = info.css(".four-story--title a").attr("href").text
+      FeatureArt.new(title, link)
+  end
+
+  def scrape_feat(article)
+    #   article_page = Nokogiri::HTML(HTTParty.get(article.link).body)
+    #   article.author = article_page.css("xxx").text
+    #   article.biopart1 = article_page.css("p")[2].text
+    #   article.biopart2 = article_page.css("p")[3].text
+    #   article.date = article_page.css(".content-header__publish-date").text
+    # end
+
+  def otherarts(page)
+    page.css(".feed-card").each do |info|
+      title = info.css(" .feed-card--title a").text
+      link = info.css(" .feed-card--title a").attr("href").text
+      Article.new(title, link)
+    end
+  end
+
   # # # scrapes each instacnce of article bio/ info
-  # def scrape_article(article)
+  # def scrape_other(article)
   #   article_page = Nokogiri::HTML(HTTParty.get(article.link).body)
+  #   article.author = article_page.css("xxx").text
   #   article.biopart1 = article_page.css("p")[2].text
   #   article.biopart2 = article_page.css("p")[3].text
   #   article.date = article_page.css(".content-header__publish-date").text
